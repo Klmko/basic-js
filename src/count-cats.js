b869c4c9caf8165915f17d4e487c0d900ca3ac27
Nofa <106711188+Klmko@users.noreply.github.com>
2022-10-18 01:58:53 +0300

    Add files via upload

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,24 +1,2 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const countCats = (catsArr) => (catsArr.toString().match(/\^\^/g) || []).length;
 
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
-
-module.exports = {
-  countCats
-};
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Extract season from given date and expose the enemy scout!
- * 
- * @param {Date | FakeDate} date real or fake date
- * @returns {String} time of the year
- * 
- * @example
- * 
- * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
- */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  } else if (!(date instanceof Date) || date.toString() == 'Invalid Date') {
+    throw 'Invalid Date';
+  } else {
+    return ['winter', 'spring', 'summer', 'autumn'][Math.floor((a.getMonth() + 1) % 12 / 3)];
+  }
 }
-
-module.exports = {
-  getSeason
-};
